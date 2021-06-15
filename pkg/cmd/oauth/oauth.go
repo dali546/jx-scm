65f5635ab42985ec634b67a073d9039090d35ac0
dali546 <35352237+dali546@users.noreply.github.com>
2021-06-15 22:30:27 +0100

    attempt to create an updated secret

--- pkg/cmd/oauth/oauth.go
+++ pkg/cmd/oauth/oauth.go
@@ -1,25 +1,29 @@
 package oauth
 
 import (
+	"context"
+
 	"github.com/jenkins-x/jx-helpers/v3/pkg/cobras/helper"
+	"github.com/jenkins-x/jx-helpers/v3/pkg/kube"
 	"github.com/jenkins-x/jx-kube-client/v3/pkg/kubeclient"
 	"github.com/jenkins-x/jx-logging/v3/pkg/log"
 	"github.com/spf13/cobra"
-	"github.com/jenkins-x/jx-git-operator/pkg/constants"
 
 	"github.com/jenkins-x/jx-git-operator/pkg/repo"
 	"github.com/pkg/errors"
 	v1 "k8s.io/api/core/v1"
+
 	// apierrors "k8s.io/apimachinery/pkg/api/errors"
-	// metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
+	"golang.org/x/oauth2/clientcredentials"
+
 )
 
 // Options the options for updating jx-boot secrets
 type Options struct {
-	kubeClient kubernetes.Interface
-	ns         string
-	selector   string
+	kubeClient    kubernetes.Interface
+	secret        *v1.Secret
 }
 
 // NewCmdUpdateSecret refreshes the jx-boot secret
@@ -37,25 +41,14 @@ func NewCmdUpdateSecret() (*cobra.Command, *Options) {
 }
 
 func (o *Options) Validate() error {
+	var err error
 	if o.kubeClient == nil {
-		f := kubeclient.NewFactory()
-		cfg, err := f.CreateKubeConfig()
+		o.kubeClient, err = kube.LazyCreateKubeClient(o.kubeClient)
 		if err != nil {
-			return errors.Wrapf(err, "failed to create kube config")
-		}
-
-		o.kubeClient, err = kubernetes.NewForConfig(cfg)
-		if err != nil {
-			return errors.Wrapf(err, "failed to create the kube client")
-		}
-
-		if o.ns == "" {
-			o.ns, err = kubeclient.CurrentNamespace()
-			if err != nil {
-				return errors.Wrapf(err, "failed to find the current namespace")
-			}
+			return errors.Wrapf(err, "failed to create kube client")
 		}
 	}
+
 	return nil
 }
 
@@ -64,5 +57,30 @@ func (o *Options) Run() error {
 	if err != nil {
 		return errors.Wrapf(err, "failed to validate options")
 	}
+
+	oldSecret, err := o.kubeClient.
+		CoreV1().
+		Secrets("jx-git-operator").
+		Get(context.Background(), "jx-boot", metav1.GetOptions{})
+
+	o.secret, err = updateSecret(oldSecret)
+	o.kubeClient.CoreV1().
+		Secrets("jx-git-operator").
+		Update(context.Background(), o.secret, metav1.UpdateOptions{})
 	return nil
+}
+
+func updateSecret(s *v1.Secret) (*v1.Secret, error) {
+	config := clientcredentials.Config{
+		ClientID:     "",
+		ClientSecret: "",
+		TokenURL:     "https://bitbucket.org/site/oauth2/access_token",
+	}
+
+	s.Data["username"] = []byte("x-token-auth")
+	token, err := config.Token(context.Background())
+	if err == nil {
+		s.Data["password"] = []byte(token.AccessToken)
+	}
+	return s, nil
 }
